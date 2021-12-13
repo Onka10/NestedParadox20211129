2a16d33f59c3837f6da6555464a69521590d07bd
Onka10 <87790609+Onka10@users.noreply.github.com>
2021-12-13 18:01:04 +0900

    Git  ignoreに画像ファイルをignoreしないよう修正 赤さんのスクリプトが消えていた問題の修正

---
+++ Assets/Akasan/Scripts/GameUIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIManager : MonoBehaviour
+{
+    [SerializeField] GameObject pausePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+    }
+    public void UnPause()
+    {
+        pausePanel.SetActive(false);
+    }
+}
