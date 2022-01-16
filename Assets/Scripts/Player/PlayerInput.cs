d4b67a6daa7fd5674910820c1c8fc9992e74fd7c
Onka10 <87790609+Onka10@users.noreply.github.com>
2022-01-16 11:47:34 +0900

    Playerの実装の続き

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -8,32 +8,55 @@ namespace NestedParadox.Players
 {
     public class PlayerInput : MonoBehaviour
     {
-        private PlayerMove _playerMove;
-        void Awake(){
-            _playerMove = GetComponent<PlayerMove>();
+        //購読される変数
+        public IObservable<Unit> OnNormalAttack => _normalAttackSubject;
+        public IObservable<Unit> OnStrongAttack => _chargeAttackSubject;
+        public IReadOnlyReactiveProperty<bool> IsJump => _jump;
+        public IReadOnlyReactiveProperty<Vector3> MoveDirection => _move;
+
+        // イベント発行に利用するSubjectやReactiveProperty
+        private readonly Subject<Unit> _normalAttackSubject = new Subject<Unit>();
+        private readonly Subject<Unit> _chargeAttackSubject = new Subject<Unit>();
+        private readonly ReactiveProperty<bool> _jump = new ReactiveProperty<bool>(false);
+        private readonly ReactiveProperty<Vector3> _move = new ReactiveProperty<Vector3>();
+
+
+
+
+
+        private void Start(){
+            //AddToでOnDestroy時にDispose()されるように登録する
+            _normalAttackSubject.AddTo(this);
+            _chargeAttackSubject.AddTo(this);
+            _jump.AddTo(this);
+            _move.AddTo(this);
+
+            //updateをobservableにする
+            this.UpdateAsObservable()
+            // Attackボタンの状態を取得 
+            .Select(_ => Input.GetMouseButtonDown(0))
+            // 値が変動した場合のみ通過
+            .DistinctUntilChanged()
+            // 最後に状態が変動してからの経過時間を付与
+            .TimeInterval()
+            .Skip(1)
+            .Subscribe(t =>
+            {
+                // 攻撃ボタンを押した瞬間のイベントは無視
+                if (t.Value) return;
+                _normalAttackSubject.OnNext(Unit.Default);
+
+                Debug.Log("攻撃");
+            }).AddTo(this);
         }
+
         void Update(){
-            if(Input.GetKey(KeyCode.D))
-            {
-                Debug.Log("移動!");
-                _playerMove.Move(1);
-            }
-            else if(Input.GetKey(KeyCode.A))
-            {
-                Debug.Log("移動!");
-                _playerMove.Move(-1);
-            }
+            // ジャンプボタン
+            _jump.Value = Input.GetKeyDown("space");
 
-            if(Input.GetKeyDown(KeyCode.Space))
-            {
-                Debug.Log("ジャンプ!");
-                _playerMove.Jump();
-            }
-
-            if (Input.GetMouseButtonDown(0))
-            {
-                Debug.Log("攻撃!");
-            }
+            // 移動入力をベクトルに変換して反映
+            // ReactiveProperty.SetValueAndForceNotifyを使うと強制的にメッセージ発行できる
+            _move.SetValueAndForceNotify(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
         }
     }
 }
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -5,34 +5,102 @@ namespace NestedParadox.Players
 {
     public class PlayerMove : MonoBehaviour
     {
-        [SerializeField] Rigidbody2D rb;
-        [SerializeField] Transform mytransform;
-        [SerializeField] float jumpPower;
-        [SerializeField] float gravityPower;
+        // 接地状態
+        public IReadOnlyReactiveProperty<bool> IsGrounded => _isGrounded;
+        // 落下中であるか
+        // public bool IsFall => _rigidbody2D.velocity.y < 0;
+
+
+        // 移動速度
+        [SerializeField] private float _dashSpeed = 3;
+        // ジャンプ速度
+        [SerializeField] private float _jumpSpeed = 5.5f;
+
 
         //行動不能
         private bool _isMoveBlock;
+        //地面の判定に使うレイ
+        private readonly RaycastHit2D[] _raycastHitResults = new RaycastHit2D[1];
+        //地面の判定
+        private readonly ReactiveProperty<bool> _isGrounded = new BoolReactiveProperty(true);
+
+
+        //外部参照
+        private PlayerCore _playerCore;
+        private Rigidbody2D _rigidbody2D;
+        private PlayerInput _playerinput;
+
+        private void Start(){
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _playerCore = GetComponent<PlayerCore>();
+            _playerinput = GetComponent<PlayerInput>();
+        }
 
         private void FixedUpdate()
         {
-            rb.AddForce(new Vector3(0, -1 * gravityPower));
+            // 接地判定処理
+            CheckGrounded();
+
+            // 上書きする移動速度の値
+            var vel = Vector3.zero;
+
+            // 操作イベントから得られた移動量
+            var moveVector = GetMoveVector();
+
+            // 移動操作を反映する
+            if (moveVector != Vector3.zero)
+            {
+                Debug.Log("移動");
+                vel = moveVector * _dashSpeed;
+            }
+
+            // ジャンプ
+            if (_playerinput.IsJump.Value)
+            {
+                Debug.Log("Jump");
+                vel += Vector3.up * _jumpSpeed;
+            }
+
+            // 重力落下分を維持する
+            vel += new Vector3(0, _rigidbody2D.velocity.y, 0);
+
+            // 速度を更新
+            _rigidbody2D.velocity = vel;
         }
 
-        public void Move(int direction)
+        // 操作イベントの値から移動量を決定する
+        private Vector3 GetMoveVector()
         {
-            if(direction == 1)
+            var x = _playerinput.MoveDirection.Value.x;
+            if (x > 0.1f)
             {
-                mytransform.localScale = new Vector3(1, 1, 1);
+                //ここで向き変更の通知をカメラに送る
+                Debug.Log("右");
+                return Vector3.right;
             }
-            else if (direction == -1)
+            else if (x < -0.1f)
             {
-                mytransform.localScale = new Vector3(-1, 1, 1);
+                return -Vector3.right;
+            }
+            else
+            {
+                return Vector3.zero;
             }
         }
 
-        public void Jump()
+        // 接地判定
+        private void CheckGrounded()
         {
-            rb.AddForce(new Vector2(0, jumpPower), ForceMode2D.Force);
+            // 地面に対してRaycastを飛ばして接地判定を行う
+            // var hitCount = Physics2D.CircleCastNonAlloc(
+            //     origin: transform.position - new Vector3(0, _characterHeightOffset, 0),
+            //     radius: 0.09f,
+            //     direction: Vector2.down,
+            //     results: _raycastHitResults,
+            //     distance: 0.01f,
+            //     layerMask: _groundMask);
+
+            // _isGrounded.Value = hitCount != 0;
         }
 
         // 移動不可フラグ
