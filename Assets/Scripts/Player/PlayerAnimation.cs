63daee94d6f115e7b4d248c8384c91992239b651
Onka10 <87790609+Onka10@users.noreply.github.com>
2022-01-24 18:42:25 +0900

    playerのバグ潰し

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -37,7 +37,12 @@ namespace NestedParadox.Players{
                 var scale = transform.localScale;
                 scale.x = moveX > 0 ? -1 : 1;
                 transform.localScale = scale;
-                //赤さんが欲している情報transformと向き
+
+                //若干傾ける
+                // Quaternion rotation;
+                // rotation = transform.rotation;
+                // rotation.z = 8f;
+                // transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 1f * Time.deltaTime);
             }
 
             _animator.SetFloat(HashSpeed, Mathf.Abs(moveX));
